063dac263bb16e83db447b473b63ee748df40971
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-09-12 15:53:11 +0800

    feat: configure tailwind to use flowbite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+
+const flowbite = require('flowbite-react/tailwind');
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+ content: [
+  './pages/**/*.{js,ts,jsx,tsx,mdx}',
+  './components/**/*.{js,ts,jsx,tsx,mdx}',
+  './app/**/*.{js,ts,jsx,tsx,mdx}',
+  flowbite.content(),
+ ],
+ theme: {
+  extend: {
+   colors: {
+    background: 'var(--background)',
+    foreground: 'var(--foreground)',
+   },
   },
-  plugins: [],
+ },
+ plugins: [flowbite.plugin()],
 };
 export default config;
